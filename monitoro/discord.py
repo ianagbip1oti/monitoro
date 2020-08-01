5d48297d0db24c4fde2b01256976cb3230875407
ianagbip1oti <ianagbip1oti@gmail.com>
2020-08-01 17:44:41 +0000

    🐛 FIX: Create user DM channels as needed

--- monitoro/discord.py
+++ monitoro/discord.py
@@ -4,3 +4,8 @@ import functools
 @functools.lru_cache
 def get_user(smalld, user_id):
     return smalld.get(f"/users/{user_id}")
+
+
+@functools.lru_cache
+def get_dm_channel(smalld, user_id):
+    return smalld.post("/users/@me/channels", {"recipient_id": user_id})
