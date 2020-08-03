b629746f244306ee02fe96c55f72504f72cdc642
ianagbip1oti <ianagbip1oti@gmail.com>
2020-08-03 14:24:54 +0000

    📦 NEW: Watch must be issued in guild shared with bot

--- monitoro/discord.py
+++ monitoro/discord.py
@@ -1,9 +1,21 @@
 import functools
+from smalld import HttpError
 
 
 @functools.lru_cache
 def get_user(smalld, user_id):
     return smalld.get(f"/users/{user_id}")
+
+
+@functools.lru_cache
+def get_guild_member(smalld, guild_id, user_id):
+    try:
+        return smalld.get(f"/guilds/{guild_id}/members/{user_id}")
+    except HttpError as e:
+        if e.response.status_code == 404:
+            return None
+        else:
+            raise
 
 
 @functools.lru_cache
