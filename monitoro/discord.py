3c78dfed1c97fac8dc9cd581f86b38009e08be36
ianagbip1oti <ianagbip1oti@gmail.com>
2020-12-07 20:58:47 +0000

    🐛 FIX: Use py38 for fmt and lint actions

--- monitoro/discord.py
+++ monitoro/discord.py
@@ -3,12 +3,12 @@ import functools
 from smalld import HttpError
 
 
-@functools.lru_cache()
+@functools.lru_cache
 def get_user(smalld, user_id):
     return smalld.get(f"/users/{user_id}")
 
 
-@functools.lru_cache()
+@functools.lru_cache
 def get_guild_member(smalld, guild_id, user_id):
     try:
         return smalld.get(f"/guilds/{guild_id}/members/{user_id}")
@@ -19,6 +19,6 @@ def get_guild_member(smalld, guild_id, user_id):
             raise
 
 
-@functools.lru_cache()
+@functools.lru_cache
 def get_dm_channel(smalld, user_id):
     return smalld.post("/users/@me/channels", {"recipient_id": user_id})
