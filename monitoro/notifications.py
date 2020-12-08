21a915945b4d8774b8f3cc78529f0936d808e4be
ianagbip1oti <ianagbip1oti@gmail.com>
2020-12-08 01:49:51 +0000

    🐛 FIX: Fix error in threading/send_dm logic

--- monitoro/notifications.py
+++ monitoro/notifications.py
@@ -14,8 +14,8 @@ class Notifications:
         if monitored_by := self.watchers.get_watchers(bot_id):
             watching = discord.get_user(self.smalld, bot_id)
 
-            def send_dm():
-                dm_channel = discord.get_dm_channel(self.smalld, user["watcher"])
+            def send_dm(user_id):
+                dm_channel = discord.get_dm_channel(self.smalld, user_id)
 
                 self.smalld.post(
                     f"/channels/{dm_channel.id}/messages",
@@ -26,7 +26,12 @@ class Notifications:
             self.scheduled_notifications[bot_id] = scheduler
 
             for user in monitored_by:
-                scheduler.enter(user.get("minutes", 0) * 60, 0, send_dm)
+                scheduler.enter(
+                    delay=user.get("minutes", 0) * 60,
+                    priority=0,
+                    action=send_dm,
+                    argument=(user["watcher"],),
+                )
 
             Thread(target=scheduler.run).start()
 
